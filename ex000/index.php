ff4a67eea3179ac21d843fe0af26738eca49bee3
Piter Gomes <142267146+pitercoding@users.noreply.github.com>
2025-06-04 21:30:27 +0200

    Olá,Mundo! PHP
    M01 Aula 17 – Primeiro script em PHP

---
+++ ex000/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ola, Mundo!</title>
+</head>
+<body>
+    <h1>
+        <?php 
+            echo "Olá, Mundo! \u{1F30E}";
+            //print "Olá, Mundo! com print";
+        ?>
+    </h1>
+    <p>Vamos nos livrar da maldição</p>
+</body>
+</html>
