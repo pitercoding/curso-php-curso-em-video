385cef32e9d90bc8b92061fce114b0aae2bb819f
Piter Gomes <142267146+pitercoding@users.noreply.github.com>
2025-06-05 21:14:21 +0200

    tipos primnitivos
    string, int, float, boolean

---
+++ ex003/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos Primitivos em PHP</title>
+</head>
+<body>
+    <h1>Teste de Tipos Primitivos</h1>
+    <?php 
+        //$num = 0x1A;
+        //echo "O valor da variável é $num";
+
+        //$v = "Piter";
+        //var_dump($v);
+
+        //$num = 3e2; // 3 x 10(2)
+        //echo "O valor é $num";
+        //var_dump($num);
+
+        //$num = (float) "950";
+        //var_dump($num);
+
+        $casado = false;
+        //var_dump($casado);
+        print "O valor para casado é $casado";
+    ?>
+</body>
+</html>
