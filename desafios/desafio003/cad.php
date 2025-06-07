1d097411f47894794633fe3cb4f8e42a4447c751
Piter Gomes <142267146+pitercoding@users.noreply.github.com>
2025-06-07 19:07:54 +0200

    ajustes Desafio 003: Conversor de Moedas básico

--- desafios/desafio003/cad.php
+++ desafios/desafio003/cad.php
@@ -9,15 +9,19 @@
 <body>
     <main>
         <h1>Conversor de Moedas v1.0</h1>
-        <?php 
-            $valorReal = $_GET["moeda"];
-            $vDolarHoje = 5.56;
-            $vConvertido = $valorReal / $vDolarHoje;
+            <?php
+                $real = $_GET["moeda"];
+                $cotacao = 5.56;
+                $dolar = $real / $cotacao;
+                
+                //Formatação de moedas com internacionalização
+                $padrão = numfmt_create("pt_BR", NumberFormatter::CURRENCY); //criando padrão. Idioma e CURRENCY
 
-            echo "<p>Seus R$ $valorReal  equivalem a <strong>US$$vConvertido</strong>.</p>";
-            echo "<p>* A cotação do dólar hoje é de <strong>US$$vDolarHoje</strong></p>";
-        ?>
-        <button><a href="javascript:history.go(-1)">Voltar</a></button>
+                echo "<p>Seus " . numfmt_format_currency($padrão, $real, "BRL") . " equivalem a <strong>" . numfmt_format_currency($padrão, $dolar, "USD") . "</strong>.</p>";
+
+                echo "* A cotação hoje é de <strong>" . numfmt_format_currency($padrão, $cotacao, "BRL") . "</strong> para <strong>1US$</strong>.";
+            ?>
+        <button onclick="javascript:history.go(-1)">Voltar</button>
     </main>
 </body>
 </html>
