e715cb930426e213f1c608ecd57bed6312035ab0
Piter Gomes <142267146+pitercoding@users.noreply.github.com>
2025-06-07 16:41:31 +0200

    corrigindo desafio2

--- desafios/desafio002/index.php
+++ desafios/desafio002/index.php
@@ -9,12 +9,13 @@
 <body>
     <main>
         <h1>Trabalhando com números aleatórios</h1>
-        <p>Gerando um número aleatório entre 0 e 100...</p>
         <?php 
-            $naleatorio = rand(1, 100);
-            echo "O valor gerado foi: <strong>$naleatorio</strong>.";
+            $min = 0;
+            $max = 100;
+            $naleatorio = mt_rand($min, $max);
+            echo "<p>Gerando um número aleatório entre $min e $max... <br>O valor gerado foi: <strong>$naleatorio</strong>.</p>";
         ?>
-        <button><a href="javascript:window.location.reload()">Gerar outro</a></button>
+        <button onclick="javascript:window.location.reload()">&#x1F504; Gerar outro</button>
     </main>
 </body>
 </html>
