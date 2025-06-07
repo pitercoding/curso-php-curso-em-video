b7641148aea7e0bc9bbce26bd23903dad11bed8b
Piter Gomes <142267146+pitercoding@users.noreply.github.com>
2025-06-07 15:32:24 +0200

    desafio 3 - Conversor de Moedas v1.0

---
+++ desafios/desafio003/cad.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ept-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Doesafio PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <h1>Conversor de Moedas v1.0</h1>
+        <?php 
+            $valorReal = $_GET["moeda"];
+            $vDolarHoje = 5.56;
+            $vConvertido = $valorReal / $vDolarHoje;
+
+            echo "<p>Seus R$ $valorReal  equivalem a <strong>US$$vConvertido</strong>.</p>";
+            echo "* A cotação do dólar hoje é de <strong>US$$vDolarHoje</strong>";
+        ?>
+        <button><a href="javascript:history.go(-1)">Voltar</a></button>
+    </main>
+</body>
+</html>
